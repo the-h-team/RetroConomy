523616b00154e6c4699336b1df553a75a9f721b5
Matthew Sanders <msanders5984@gmail.com>
2021-04-26 17:04:14 -0400

    Add in-development headers

--- src/main/java/com/github/sanctum/retro/util/ATM.java
+++ src/main/java/com/github/sanctum/retro/util/ATM.java
@@ -1,3 +1,11 @@
+/*
+ *  Copyright 2021 Sanctum <https://github.com/the-h-team>
+ *
+ *  This file is part of RetroConomy.
+ *
+ *  This software is currently in development and its licensing has not
+ *  yet been chosen.
+ */
 package com.github.sanctum.retro.util;
 
 import com.github.sanctum.labyrinth.data.container.DataContainer;
